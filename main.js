f3e3a9de5041d9fa4577df49ee25ef587abc5ef3
Ian <ian@student.dev-island.org>
2023-08-17 09:54:14 -1000

    added validation of password in JS

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
-let myInput = document.getElementById("password")
-let confirmInput = document.getElementById("confirm_password")
+const form = document.getElementById("mailingList");
+const myInput = document.getElementById("password");
+const confirmInput = document.getElementById("confirm_password");
 
-// when the user clicks on the confirm password field, show the message & give box outline color red  
-confirmInput.onfocus = function() {
+// When the passwords dont match, the message appears.
+function checkPassword2() {
+  if (myInput.value !== confirmInput.value) {
+    confirmInput.setCustomValidity("Passwords do not match.");
     document.getElementById("message").style.display = "block";
-}
-
-// when the user clicks on the back outside the password field, hide the message 
-confirmInput.onblur = function() {
+    confirmInput.style.border = "1px solid red";
+    myInput.style.border = "1px solid red";
+  } else {
+    confirmInput.setCustomValidity("");
+    document.getElementById("submit").classList.add("glow-on-validation");
     document.getElementById("message").style.display = "none";
-    
+    confirmInput.style.border = "1px solid green";
+    myInput.style.border = "1px solid green";
+  }
 }
 
+myInput.addEventListener("input", checkPassword2);
+confirmInput.addEventListener("input", checkPassword2);
 
-
+form.addEventListener("submit", function (event) {
+  if (!form.checkValidity()) {
+    event.preventDefault();
+  }
+});
